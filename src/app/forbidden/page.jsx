a6594f48ba488d75006f8a7d26f8c9c60394e55b
Aditta Chakraborty <cseaditta@gmail.com>
2026-01-13 02:06:18 +0600

    forbidden access implemented in proxy

---
+++ src/app/forbidden/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ForbiddenPage = () => {
+    return (
+        <div>
+            <h2 className="py-20 text-8xl text-center">
+                Forbidden Access
+            </h2>
+        </div>
+    );
+};
+
+export default ForbiddenPage;
--- src/proxy.js
+++ src/proxy.js
@@ -7,6 +7,10 @@ const privateRoutes = [
     "/secret",
 ];
 
+const adminRoutes = [
+    "/dashboard"
+];
+
 // This function can be marked `async` if using `await` inside
 export async function proxy(req) {
     const token = await getToken({ req });
@@ -15,11 +19,18 @@ export async function proxy(req) {
     const isUser = token?.role === "user";
     const isAdmin = token?.role === "admin";
     const isPrivate = privateRoutes.some(route => reqPath.startsWith(route));
+    const isAdminRoute = adminRoutes.some(route => reqPath.startsWith(route));
 
+    // logic for private routes
     if (!isAuthenticated && isPrivate) {
         const loginUrl = new URL("/api/auth/signin", req.url);
         loginUrl.searchParams.set("callback", reqPath);
         return NextResponse.redirect(loginUrl);
+    }
+
+    // logic for admin routes
+    if (isAuthenticated && !isAdmin && isAdminRoute) {
+        return NextResponse.redirect(new URL("/forbidden", req.url));
     }
 
     // return NextResponse.redirect(new URL("/", req.url))
